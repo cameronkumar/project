9b5f2c77e8646967562117ad7b2ca5cf11cfeead
Cameron Kumar <sc12ctk@comp-pc3081.leeds.ac.uk>
2016-03-05 18:19:44 +0000

    wrote some of the code for class methods, slight changes to header

--- vis.cpp
+++ vis.cpp
@@ -1,2 +1,68 @@
-/* Function definitions for the visualisation class of the non-Euclidean 3D
-   visualisation software. All member function code is defined here */
+/**
+   Function definitions for the visualisation class of the non-Euclidean 3D
+   visualisation software. All member function code is defined here 
+   
+   @author Cameron Kumar
+   @version 1.0 5/3/16
+*/
+
+#include "vis.h"
+#include <QMouseEvent>
+#include <QKeyEvent>
+#include <GL/glut.h>
+
+/**
+   class constructor
+   
+   @param parent parent class, inherit from there 
+*/
+vis::vis(QWidget *parent): QGLWidget(parent) {} // simple constuctor
+
+/**
+   initialises environment for OpenGL rendering when instance called
+*/
+vis::initializeGL() {
+
+	glClearColor(0.0,0.0,0.0,0.0); // white background
+	glOrtho(-1.0,1.0,-1.0,1.0,-1.0,1.0); // sets the clipping plane
+	
+	glEnable(GL_DEPTH_TEST); // allows for depth comparison when renderin
+	
+	// setting up lighting
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0); // creating one light, light0
+	GLfloat fPosition[4] = {0.0, 0.0, 0.0, 1.0}; // light position
+	glLightfv(GL_LIGHT0, GL_POSITION, fPosition); // setting position
+	// now to specify ambient, diffuse, and specular intensities, all white
+	GLfloat fiAmbient[4] = {0.4, 0.4, 0.4, 1.0};
+	glLightfv(GL_LIGHT0, GL_AMBIENT, fiAmbient); 
+	GLfloat fiDiffuse[4] = {0.6, 0.6, 0.6, 1.0};
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, fiDiffuse);
+	GLfloat fiSpecular[4] = {1.0, 1.0, 1.0, 1.0};
+	glLightfv(GL_LIGHT0, GL_SPECULAR, fiSpecular);
+	
+}
+
+/**
+   changes size of viewport when widget resized
+ 
+   @param w the new width of the widget
+   @param h the new height of the widget
+*/
+vis::resizeGL(int w, int h) {
+
+	// resize the viewport to that of the new widget dimensions
+	glViewport(0,0,w,h);
+	
+}
+
+/**
+   draw a new frame
+*/ 
+vis::paintGL() {
+
+	// clearing screen first
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+
+}
--- vis.h
+++ vis.h
@@ -1,3 +1,68 @@
-/* Class declaration for the visualisation part of the non-Euclidean 3D
-   visualisation software. An instance of this will appear in the GUI */
+/**
+   Class declaration for the visualisation part of the non-Euclidean 3D
+   visualisation software. An instance of this will appear in the GUI 
+   
+   @author Cameron Kumar
+   @version 1.0 5/3/16
+*/
 
+#include <QGLWidget> // allows us to define a Qt OpenGL widget
+
+// simplified calling functions from the std class
+using namespace std;
+
+// header guard, stops repeated inclusion of this header by the compiler
+#ifndef __VIS_H__
+#define __VIS_H__
+
+// defining our visualisation class as subclass of QGLWidget
+class vis: public QGLWidget {
+
+	public:
+	
+	/**
+	   class constructor
+	   
+	   @param parent parent class, inherit from there 
+	*/
+	vis(QWidget *parent);
+
+	protected:
+	
+	/**
+	   initialises environment for OpenGL rendering when instance called
+	*/
+	void initializeGL();
+	
+	/**
+	   changes size of viewport when widget resized
+	  
+	   @param w the new width of the widget
+	   @param h the new height of the widget
+	*/
+	void resizeGL(int w, int h);
+	
+	/**
+	   draw a new frame
+	*/ 
+	void paintGL();
+	
+	/**
+	   interaction handling for when a mouse button is pressed, used for 
+	   camera movement
+	   
+	   @param event information about the mouse button click
+	*/
+	void mousePressEvent(QMouseEvent *event);
+	
+	/**
+	   interaction handling for when a button on the keyboard is pressed,  
+	   used for camera zooming
+	   
+	   @param event information about the key pressed
+	*/
+	void keyPressEvent(QKeyEvent *event);
+
+}; // end of class
+
+#endif // ends our header guard
